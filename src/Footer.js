1e6e9c573c59b97a58782f2b07031880d483c7d2
dipen305 <hdipen@gmail.com>
2022-11-03 08:43:59 +0530

    React Day 2 initial commit

--- src/Footer.js
+++
@@ -1,5 +0,0 @@
-const Header = () =>{
-    return (<h2>Footer Component Loaded!</h2>);
-}
- 
-export default Header;
--- src/Header.js
+++
@@ -1,6 +0,0 @@
-const Header = ()=> {
-    return (<h1>Header Component Loaded!</h1>)
-
-}
-
-export default Header;
